dac4e3122de7060a8ec9ed86b5307324a98c6380
ArtiomHryniuk <60979235+ArtiomHryniuk@users.noreply.github.com>
2020-02-18 13:57:56 +0200

    Add file

---
+++ 11.py
@@ -0,0 +1,9 @@
+a = int(input('Введіть A: '))
+if (a < 0) or (a > 1000000):
+    print('Не в діапазоні')
+else:
+    a = str(a)
+    sum = 0
+    for x in range(a):
+        sum+=int(x)
+    print(f'Сума: {sum}')
