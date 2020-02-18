ec26fae1ae6981ed8814038a5018864170408328
Bohdan <58623074+nc6d@users.noreply.github.com>
2020-02-18 14:13:20 +0200

    Add files via upload

---
+++ Program.15.py
@@ -0,0 +1,79 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:2
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <module>
+    arr = [int(input(i ,': ')) for i in range(N)]
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <listcomp>
+    arr = [int(input(i ,': ')) for i in range(N)]
+TypeError: input expected at most 1 arguments, got 2
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:2
+01
+12
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:2
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <module>
+    arr = [int(input(i,':')) for i in range(1,N+1)]
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <listcomp>
+    arr = [int(input(i,':')) for i in range(1,N+1)]
+TypeError: input expected at most 1 arguments, got 2
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:2
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <module>
+    arr = [int(input('',i,':')) for i in range(1,N+1)]
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 5, in <listcomp>
+    arr = [int(input('',i,':')) for i in range(1,N+1)]
+TypeError: input expected at most 1 arguments, got 3
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:4
+11
+21
+31
+41
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 12, in <module>
+    for n in arr_set_f:
+RuntimeError: Set changed size during iteration
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:4
+11
+21
+31
+41
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 12, in <module>
+    for n in arr_set_f:
+RuntimeError: Set changed size during iteration
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:4
+11
+22
+33
+44
+Traceback (most recent call last):
+  File "C:\Users\Dell\Desktop\Новая папка (2)\Program16.py", line 9, in <module>
+    for i in range():
+TypeError: range expected 1 arguments, got 0
+>>> 
+======== RESTART: C:\Users\Dell\Desktop\Новая папка (2)\Program16.py ========
+Введіть кількість елементів:4
+11
+22
+33
+44
+2
+4
+1
+3
+>>> 
