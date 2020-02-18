5e0ad7cdc16bb6997b2eb96bcb72972304c56357
ArtiomHryniuk <60979235+ArtiomHryniuk@users.noreply.github.com>
2020-02-18 14:01:43 +0200

    Delete 11.py

--- 11.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Введіть A: '))
-if (a < 0) or (a > 1000000):
-    print('Не в діапазоні')
-else:
-    a = str(a)
-    sum = 0
-    for x in range(a):
-        sum+=int(x)
-    print(f'Сума: {sum}')
